a1c75b4deca6a20d8bab5ec993d226a8935be653
James Morris <morri398@purdue.edu>
2020-01-17 18:45:42 -0500

    Initial Commit

---
+++ tipyy2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipyy2.0
+//
+//  Created by James Morris on 1/17/20.
+//  Copyright © 2020 James Morris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
