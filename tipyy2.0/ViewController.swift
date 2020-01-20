ee6cd814c2ffbe88a91f3c2e1c4211212e8ce310
James Morris <morri398@purdue.edu>
2020-01-19 22:30:53 -0500

    Tip Calculator 2.0 1.19.20

--- tipyy2.0/ViewController.swift
+++ tipyy2.0/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipCalc: UILabel!
+    @IBOutlet weak var totalCalc: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true) // Hide Keyboard when hitting screen
+        
+    }
+    @IBAction func calcTip(_ sender: Any) {
+        let bill = Double(billField.text!) ?? 0
+        // Get Bill Amount if not a double entry fails, good fail safe
+        let tipPs = [0.15, 0.2, 0.25]
+        let tip = bill * tipPs[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Caclulate the Tip and Total
+        tipCalc.text = String(format: "$%.2f", tip)
+        totalCalc.text = String(format: "$%.2f", total)
+        
+        //Update the Tip and Total Labels
+    }
+    
 }
 
